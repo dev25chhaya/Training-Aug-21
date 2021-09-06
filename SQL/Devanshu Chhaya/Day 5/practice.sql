638bb5069062205d2562f623c94bc288b993edde
Devanshu Chhaya <drchhaya25@gmail.com>
2021-09-06 09:00:06 +0530

    REFS:#551 and Fixes:551

--- SQL/Devanshu Chhaya/Day 5/practice.sql
+++ SQL/Devanshu Chhaya/Day 5/practice.sql
@@ -1,25 +1,33 @@
 
-use [AdventureWorksLT2017]
+USE [AdventureWorksLT2017]
 
-select * from [SalesLT].[Product]
-select * from[SalesLT].[ProductCategory]
-select * from [SalesLT].[Customer]
-select * from[SalesLT].[CustomerAddress]
+SELECT * FROM [SalesLT].[Product]
+SELECT * FROM [SalesLT].[ProductCategory]
+SELECT * FROM [SalesLT].[Customer]
+SELECT * FROM [SalesLT].[CustomerAddress]
 
-select  c.FirstName,c.CustomerID,a.CustomerID,a.AddressID from [SalesLT].[Customer] as c left join [SalesLT].[CustomerAddress] as a on a.customerid=c.customerid
+SELECT c.FirstName,c.CustomerID,a.CustomerID,a.AddressID 
+FROM [SalesLT].[Customer] AS c LEFT JOIN [SalesLT].[CustomerAddress] AS a 
+ON a.customerid=c.customerid
 
 
-select c.FirstName,c.CompanyName,a.AddressType,a.CustomerID,c.CustomerID from [SalesLT].[Customer] as c join [SalesLT].[CustomerAddress] as a on c.customerid=a.customerid
+SELECT c.FirstName,c.CompanyName,a.AddressType,a.CustomerID,c.CustomerID 
+FROM [SalesLT].[Customer] AS c LEFT JOIN [SalesLT].[CustomerAddress] AS a 
+ON c.customerid=a.customerid
 
-select c.AddressID ,a.FirstName,a.CustomerID,c.CustomerID from [SalesLT].[CustomerAddress] as c right join [SalesLT].[Customer] as a on a.customerid=c.customerid
+SELECT c.AddressID ,a.FirstName,a.CustomerID,c.CustomerID 
+FROM [SalesLT].[CustomerAddress] AS c RIGHT JOIN [SalesLT].[Customer] AS a 
+ON a.customerid=c.customerid
 
-select a.AddressID ,c.FirstName from [SalesLT].[Customer] as c full join [SalesLT].[CustomerAddress] as a on a.customerid=c.customerid
+SELECT a.AddressID ,c.FirstName 
+FROM [SalesLT].[Customer] AS c FULL JOIN [SalesLT].[CustomerAddress] AS a 
+ON a.customerid=c.customerid
 
 
 ----- Self Join
 
 
-CREATE TABLE EMPLOYEE
+CREATE TABLE EMPLOYEE_
 (	ID INT PRIMARY KEY,
    FULLNAME VARCHAR(20),
    SALARY INT,
@@ -27,17 +35,41 @@ CREATE TABLE EMPLOYEE
 
 )
 DROP TABLE EMPLOYEE
-INSERT INTO EMPLOYEE VALUES 
+INSERT INTO EMPLOYEE_ VALUES 
 (1,'DEV CHHAYA',25000,null),
 (2,'RAJ',25000,1),
 (3,'MEET',20000,1),
 (4,'JAY',23000,2),
 (5,'DEEP',24000,2);
 
-SELECT * FROM EMPLOYEE
+SELECT * FROM EMPLOYEE_
 
-SELECT e.ID,e.FullName,e.managerID,m.fullname as'ManagerName' FROM EMPLOYEE e JOIN Employee m on e.managerid=m.ID
+SELECT e.ID,e.FullName,e.managerID,m.fullname AS'ManagerName' FROM EMPLOYEE_ e JOIN EMPLOYEE_ m ON e.managerid=m.ID
 
-SELECT e.ID,e.FullName,e.managerID,m.fullname as'ManagerName' FROM EMPLOYEE e left JOIN Employee m on e.managerid=m.ID
+SELECT e.ID,e.FullName,e.managerID,m.fullname AS'ManagerName' FROM EMPLOYEE_ e LEFT JOIN EMPLOYEE_ m ON e.managerid=m.ID
 
-SELECT e.ID,e.FullName,e.managerID,m.fullname as'ManagerName' FROM EMPLOYEE e right JOIN Employee m on e.managerid=m.ID
+SELECT e.ID,e.FullName,e.managerID,m.fullname AS'ManagerName' FROM EMPLOYEE_ e RIGHT JOIN Employee_ m ON e.managerid=m.ID
+
+
+----- CROSS JOIN
+
+CREATE TABLE Meals(MealName VARCHAR(100))
+
+CREATE TABLE Drinks(DrinkName VARCHAR(100))
+
+INSERT INTO Drinks
+VALUES('Orange Juice'), ('Tea'), ('Cofee')
+
+INSERT INTO Meals
+VALUES('Pitzza'), ('Sand wich'), ('Barger')
+
+SELECT * FROM Meals
+
+SELECT * FROM Drinks
+
+SELECT * FROM Meals 
+CROSS JOIN Drinks
+
+SELECT CONCAT_WS('-',MealName,DrinkName) AS MenuList
+FROM Meals CROSS JOIN 
+Drinks
