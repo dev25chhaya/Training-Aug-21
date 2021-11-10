3c4482d00d8d1abb77ddb6bdb6cd23051721946f
dev25chhaya <drchhaya25@gmail.com>
2021-11-10 10:36:50 +0530

    REFS:#2008 and Fixes:#2008 (Practiced async, await and extension method)

---
+++ C#/Devanshu Chhaya/Day9/NewMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionMethod
+{
+    static class NewMethod
+    {
+        public static void M4(this Practice p)
+        {
+            Console.WriteLine("4th method");
+        } 
+
+        public static void M5(this Practice p, string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Practice p = new Practice();
+            p.M1();
+            p.M2();
+            p.M3();
+            p.M4();
+            p.M5("5th method");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Devanshu Chhaya/Day9/extensionmethod.cs
@@ -0,0 +1,34 @@
+﻿using extensionMethod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionMethod
+{
+    class Practice
+    {
+
+        // Method 1
+        public void M1()
+        {
+            Console.WriteLine("Method Name: M1");
+        }
+
+        // Method 2
+        public void M2()
+        {
+            Console.WriteLine("Method Name: M2");
+        }
+
+        // Method 3
+        public void M3()
+        {
+            Console.WriteLine("Method Name: M3");
+        }
+
+    }
+
+}
+
